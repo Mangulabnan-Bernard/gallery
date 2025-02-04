6ca3a3389d0bb6f5ce016a184dd46bbdf1de2bbb
Mangulabnan-Bernard <adaiki287@gmail.com>
2025-02-04 00:51:26 -0800

    add authentication

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 - [✔] Scaffold with Basic UI with Mock Data
 - [✔] Setup Database (PostgreSQL)
 - [✔] Attach Database to UI
+- [✔] Add Authentication (w/ Clerk)
+- [] Add Image Upload
+- [] "taint" (Server-Only)
+- [] Error Management(w/ Sentry)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,55 @@
 import Link from "next/link";
 import Image from "next/image";
 import { db } from "~/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images(){
   const images = await db.query.images.findMany({
     orderBy: (model, {asc}) => asc (model.id),
     
   });
+  return (
+    <><div className="flex justify-center gap-4 mb-6">
+
+    </div><div className="flex flex-wrap gap-4 justify-center">
+
+
+        {images.map((image) => (
+          <div
+            key={image.id}
+            className="w-48 overflow-hidden border-2 border-black cursor-pointer flex flex-col items-center"
+
+          >
+            {/* try 1" */}
+
+            <img
+              src={image.url}
+              alt={`Image ${image.id}`}
+              width={222}
+              height={223}
+              className="object-cover w-full h-48" />
+            <div className="mt-2 text-center text-sm font-medium">{image.name}</div>
+          </div>
+        ))}
+      </div></>
+    
+  );
+}
+
+export default async function HomePage() {
+ 
   
 
   return (
     <main>
-      <div className="flex justify-center gap-4 mb-6">
-        {/* Button for "All Photos" */}
-        <Link href="/" passHref>
-          <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700">
-            All Photos
-          </button>
-        </Link>
-
-        {/* Button for "Favorites" */}
-      </div>
-    
-
-      <div className="flex flex-wrap gap-4 justify-center">
-
- 
-        {images.map((image) => (
-          <div 
-            key={image.id}
-            className="w-48 overflow-hidden border-2 border-black cursor-pointer flex flex-col items-center"
-           
-          >
-             {/* try 1" */}
-             
-              <img
-                src={image.url}
-                alt={`Image ${image.id}`}
-                width={222}
-                height={223}
-                className="object-cover w-full h-48"
-                />
-            <div className="mt-2 text-center text-sm font-medium">{image.name}</div>
-            </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2x1 text-center">Please Sign In Above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
     </main>
   );
 }
